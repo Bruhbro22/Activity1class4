c519c4ae7b13abd0b8d548f6435920fc8861a39b
Bruhbro22 <donmadhu411@gmail.com>
2026-06-05 16:42:48 -0500

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+print("I wanna find the average of all my grades!")
+math=94
+english=89
+science=96
+computer=95
+geography=93
+sum=math+english+science+computer+geography
+print("Sum is: ",sum)
+avg=sum/5
+print("Avg is: ",avg)
